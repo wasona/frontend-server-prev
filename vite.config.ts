886d5b8adb8ab98eac077f5a9626fdbb7cf0d201
Younghyun Ji <younghyun1@gmail.com>
2024-10-14 20:12:30 +0900

    [feature] removed https for now; nginx will handle it

--- vite.config.ts
+++ vite.config.ts
@@ -5,25 +5,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const key_path: string | undefined = process.env.PRIVKEY_PATH;
-const cert_path: string | undefined = process.env.FULLCHAIN_PATH;
-
-if (!key_path) {
-  throw new Error("PRIVKEY_PATH is undefined");
-}
-if (!cert_path) {
-  throw new Error("FULLCHAIN_PATH is undefined");
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true, // listens on all addresses
-    port: 443,
-    https: {
-      key: fs.readFileSync(key_path),
-      cert: fs.readFileSync(cert_path),
-    },
   },
   plugins: [react()],
 });
